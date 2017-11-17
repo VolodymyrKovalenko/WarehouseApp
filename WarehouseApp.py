378a3a8cd5854e920c7a99ffa66d925c6bf5bea9
volodymyr <whitelol888@gmail.com>
2017-11-17 14:03:54 +0200

    add main signUp and LogIn function and add some config

--- WarehouseApp.py
+++ WarehouseApp.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:234344@localhost:3306/new1_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    __tablename__ = 'example'
+    id = db.Column('id', db.INTEGER, primary_key=True)
+    data = db.Column('data', db.Unicode)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/SignUp')
+def registrarion():
+    return render_template('registration.html')
+
+@app.route('/LogIn')
+def login():
+    return render_template('singIn.html')
 
 @app.route('/main')
 def main_page():
