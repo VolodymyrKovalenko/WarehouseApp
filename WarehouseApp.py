3f477d388a0c10040ea33c84db0583ecd818f0eb
volodymyr <whitelol888@gmail.com>
2017-11-18 19:19:42 +0200

    create all tables in db of my warehouse product using orm

--- WarehouseApp.py
+++ WarehouseApp.py
@@ -25,30 +25,55 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.INTEGER, primary_key=True)
     name = db.Column(db.String(45), unique=True)
-    material = relationship("Material", back_populates="category")
+    fasons = relationship('Fason', backref='category', lazy='dynamic')
 
-class Material(db.Model):
-    __tablename__ = 'material'
+associate_table1 = db.Table('association1',
+                            db.Column('id', db.INTEGER, db.ForeignKey('fason.id')),
+                            db.Column('id', db.INTEGER, db.ForeignKey('brand.id'))
+                            )
+
+class Fason(db.Model):
+    __tablename__ = 'fason'
     id = db.Column(db.INTEGER, primary_key=True)
     name = db.Column(db.String(45), unique=True)
     category_id = db.Column(db.INTEGER, db.ForeignKey('category.id'))
-    category = relationship("Category", back_populates="material")
+    brands = db.relationship('Brand', secondary=associate_table1, backref=db.backref('brands', lazy='dynamic'))
 
-admin = User('Ostap16','i44easy99labs57','admin3@example.com','dydyaStopa3')
+
+class Brand(db.Model):
+    __tablename__ = 'brand'
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(45), unique=True)
+    model_numbers = relationship('Model_number', backref='brand', lazy='dynamic')
+
+class Model_number(db.Model):
+    __tablename__ = 'model_number'
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(45),unique=True)
+    brand_id = db.Column(db.INTEGER,db.ForeignKey('brand.id'))
+
+
+
+
+
+
+#admin = User('Ostap16','i44easy99labs57','admin3@example.com','dydyaStopa3')
 
 db.create_all() # In case user table doesn't exists already. Else remove it.
 
-db.session.add(admin)
+#db.session.add(admin)
 
 db.session.commit() # This is needed to write the changes to database
 
-User.query.all()
+#User.query.all()
 
-User.query.filter_by(username='admin').first()
+#User.query.filter_by(username='admin').first()
 
 
 
