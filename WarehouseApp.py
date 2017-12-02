9329d285c8755129f3026dfdbd53a6f58ef0f676
volodymyr <whitelol888@gmail.com>
2017-12-02 19:37:39 +0200

    fixed dropdown and ajax query to backend

--- WarehouseApp.py
+++ WarehouseApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, json
+from flask import Flask, render_template, request, redirect, url_for, session, json,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha256_crypt
 from sqlalchemy.orm import query
@@ -63,16 +63,12 @@ def main_page():
     curent_id = User.query.filter(User.login == sesion_user_login).first()
     curent_id = curent_id.id
 
-
-    # join_table = conn.execute("""SELECT category, fason,brand,model,quantity,date_adoption,date_issue FROM application_receipt
-    #             JOIN complect on application_receipt.complect_id = complect.id""")
     join_table = db.session\
         .query(Application_receipt, Complect)\
         .join(Complect)\
         .filter(Application_receipt.provider_id == curent_id)
 
     conn.close()
-
 
     return render_template('mainPage.html',curent_user=sesion_user_login,first_table_result =  join_table)
 
@@ -81,55 +77,90 @@ def main_page():
 
 @app.route('/receipt',methods=['GET','POST'])
 def receipt_application():
+    print('work_receipt')
     form = ReceiptForm(request.form)
     conn = db.engine.connect()
-    category = form.category_opt.data
-    fason = form.fason_opt.data
+    session_user_login = session['curent_user']
+
+    categories_names = db.session.query(Category)
+
+    fason_names = db.session.query(Fason)
+
+
 
     if request.method == 'POST' and form.validate():
 
-        price = form.price.data
-        fason = form.fason.data
-        brand = form.brand.data
-        model = form.model.data
+        category_name = form.category_opt.data
+        category_price = form.price.data
 
-        quantity = form.quantity.data
-        date_adoption = form.date_adoption.data
-        date_issue = form.date_issue.data
+        fason_name = form.fason_opt.data
+        fason_category_id = None
 
-        session_user_login = session['curent_user']
+        complect_category_id = None
+        complect_brand_id = None
+        complect_model_id = None
 
-        provider_id = User.query.filter_by(login=session_user_login).first().id
+        brand_name = form.brand.data
+        model_name = form.model.data
+
+        app_quantity = form.quantity.data
+        app_date_adoption = form.date_adoption.data
+        app_date_issue = form.date_issue.data
+        app_confirmed = False
+        app_provider_id = User.query.filter_by(login=session_user_login).first().id
 
         #date_adoption = now().format('YYYY-MM-DD')
-        category_receipt_app = Category(category,price)
+
+        #category_receipt_app = Category(category_name,category_price)
+        #fason_app = Fason(fason_name,fason_category_id)
+        complect_app = Complect(complect_category_id,complect_brand_id,complect_model_id)
+        brand_app = Brand(brand_name)
+        model_app = Model(model_name)
+
         #db.session.add(category_receipt_app)
-        #db.session.commit()
+        #db.session.add(fason_app)
+        db.session.add(complect_app)
+        db.session.add(brand_app,model_app)
+        db.session.commit()
 
-        #fason_receipt_app == Fason(fason,)
-
-        #curent_id = User.query.filter(User.login == sesion_user_login).first()
-        #curent_id = curent_id.id
 
         complect_id = conn.execute('select id from complect order by id desc limit 1')
         complect_id = complect_id.fetchone()
         complect_id = complect_id[0]
-        #complect_id +=1
-        confirmed = True
 
 
-        receipt_app = Application_receipt(complect_id,quantity,date_adoption,date_issue,provider_id,confirmed)
+        receipt_app = Application_receipt(complect_id,app_quantity,app_date_adoption,app_date_issue,app_provider_id,app_confirmed)
         db.session.add(receipt_app)
         db.session.commit()
         conn.close()
 
         return redirect(url_for('main_page'))
 
-    return render_template('applicationForReceipt.html', form = form)
+    return render_template('applicationForReceipt.html', form = form, categoty_html = categories_names,fason_html=fason_names)
 
-#@app.route('/admin',methods=['GET','POST'])
-#def admin_page():
+@app.route('/handler1',methods=['POST'])
+def AjaxCategory():
+    category = request.form['categor']
+    session['curent_category'] = category
+    print('my category is:',category)
+    return json.dumps({'status': 'OK','brand': category})
 
+
+
+
+
+
+# @app.route('/Test1')
+# def signUp():
+#     return render_template('Test1.html'), jsonify({'key':'it work'})
+#
+# @app.route('/AloUser', methods=['POST'])
+# def Obrabotka():
+#     print('work1')
+#     user =  request.form['username']
+#     password = request.form['password']
+#     print(user)
+#     return json.dumps({'status':'OK','user':user,'pass':password})
 
 
 
